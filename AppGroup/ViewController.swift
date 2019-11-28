17a5066fc410be1d8b5d15b5131c5abf0c7659a3
Gwendal Roué <gr@pierlis.com>
2019-11-28 20:20:46 +0100

    Initial Commit

---
+++ AppGroup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppGroup
+//
+//  Created by Gwendal Roué on 28/11/2019.
+//  Copyright © 2019 Gwendal Roué. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
